62a35ef41d8d53b108a03389cca531b2bc26584b
EazyWu <wuyiji199632@gmail.com>
2025-05-14 13:55:06 +0100

    created debug helper for future debugging activities

--- Warrior/Source/Warrior/Private/Character/WarriorHeroCharacter.cpp
+++ Warrior/Source/Warrior/Private/Character/WarriorHeroCharacter.cpp
@@ -3,3 +3,11 @@
 
 #include "Character/WarriorHeroCharacter.h"
 
+#include "WarriorDebugHelper.h"
+
+
+void AWarriorHeroCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	Debug::Print(TEXT("Working!"));
+}
