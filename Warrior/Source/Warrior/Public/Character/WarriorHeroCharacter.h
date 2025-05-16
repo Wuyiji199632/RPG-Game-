dff6debb622ec76d8e61b3447b6ad5f13949050a
EazyWu <wuyiji199632@gmail.com>
2025-05-16 04:37:21 +0100

    Completed movement logics for both the character and the follow camera

--- Warrior/Source/Warrior/Public/Character/WarriorHeroCharacter.h
+++ Warrior/Source/Warrior/Public/Character/WarriorHeroCharacter.h
@@ -6,6 +6,11 @@
 #include "Character/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+class UDataAsset_InputConfig;
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -14,7 +19,35 @@ class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
 
+public:
+	AWarriorHeroCharacter();
 protected:
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual void BeginPlay() override;
 	
+private:
+
+#pragma region Components
+
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Camera",meta=(AllowPrivateAccess="true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+	
+#pragma endregion
+
+
+	
+	
+#pragma region Inputs
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "CharacterData", meta = (AllowPrivateAccess = "true"))
+	UDataAsset_InputConfig* InputConfigDataAsset;
+
+	void Input_Move(const FInputActionValue& InputActionValue);
+	void Input_Look(const FInputActionValue& InputActionValue);
+	
+
+#pragma endregion
 };
